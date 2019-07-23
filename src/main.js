c137a044760fb2e516f33a0ebe2e4a0a5f280e38
neo <1140356556@qq.com>
2019-07-23 18:42:39 +0800

    home header

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
+
+// 解决移动端300毫秒点击延迟问题
+import fastClick from 'fastclick'
+
+// 统一各品牌手机的默认样式
+import 'styles/reset.css'
+// 解决移动端一像素边框问题（通过 transform: scale）
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 
+fastClick.attach(document.body)
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: {App},
+    template: '<App/>'
 })
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div class="header">
+            <div class="header-left"><i class="iconfont iconback"></i></div>
+            <div class="header-input">
+                <i class="iconfont iconsearch"></i>
+                输入城市/景点/游玩主题</div>
+            <div class="header-right">
+                城市
+                <i class="iconfont iconunfold"></i>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeHeader'
+    }
+</script>
+
+<style scoped lang="stylus">
+    @import '~styles/variables.styl';
+    .header
+        display: flex;
+        height: .88rem;
+        line-height: .88rem;
+        background-color: $bgColor;
+        color: #fff;
+        .header-left
+            width: .42rem;
+            float: left;
+            margin-left: .2rem;
+            .iconback
+                font-size: .4rem;
+        .header-input
+            flex: 1;
+            height: .64rem;
+            line-height: .64rem;
+            margin-top: .12rem;
+            margin-left: .2rem;
+            padding-left: .2rem;
+            background-color: #fff;
+            border-radius: .1rem;
+            color: #ccc;
+        .header-right
+            width: 1.5rem;
+            float: right;
+            text-align: center;
+</style>
